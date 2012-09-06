6901dd28152f3cd39b3eadffffce5a5245200864
Kieren Davies <kieren@kierendavies.co.za>
2012-09-06 16:33:33 +0200

    implement contact picker

--- src/za/co/kierendavies/pleasecall/PleaseCall.java
+++ src/za/co/kierendavies/pleasecall/PleaseCall.java
@@ -1,18 +1,27 @@
 package za.co.kierendavies.pleasecall;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PleaseCall extends Activity {
-    TextView title;
+    private static final int CONTACT_PICKER_RESULT = 1001;
+
     EditText editText;
     /**
      * Called when the activity is first created.
@@ -38,7 +47,69 @@ public class PleaseCall extends Activity {
     }
 
     public void selectContact(View view) {
+        startActivityForResult(new Intent(Intent.ACTION_PICK,
+                ContactsContract.Contacts.CONTENT_URI),
+                CONTACT_PICKER_RESULT);
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // based on http://stackoverflow.com/a/6155690
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CONTACT_PICKER_RESULT:
+                    Cursor cursor = null;
+                    String phoneNumber = "";
+                    List<String> allNumbers = new ArrayList<String>();
+                    int phoneIdx = 0;
+                    try {
+                        Uri result = data.getData();
+                        String id = result.getLastPathSegment();
+                        cursor = getContentResolver().query(Phone.CONTENT_URI, null, Phone.CONTACT_ID + "=?", new String[] { id }, null);
+                        phoneIdx = cursor.getColumnIndex(Phone.DATA);
+                        if (cursor.moveToFirst()) {
+                            while (cursor.isAfterLast() == false) {
+                                phoneNumber = cursor.getString(phoneIdx);
+                                allNumbers.add(phoneNumber);
+                                cursor.moveToNext();
+                            }
+                        } else {
+                            //no results actions
+                        }
+                    } catch (Exception e) {
+                        //error actions
+                    } finally {
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+
+                        final CharSequence[] items = allNumbers.toArray(new String[allNumbers.size()]);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(PleaseCall.this);
+                        builder.setTitle("Choose a number");
+                        builder.setItems(items, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int item) {
+                                String selectedNumber = items[item].toString();
+                                selectedNumber = selectedNumber.replace("-", "");
+                                editText.setText(selectedNumber);
+                            }
+                        });
+                        AlertDialog alert = builder.create();
+                        if(allNumbers.size() > 1) {
+                            alert.show();
+                        } else {
+                            String selectedNumber = phoneNumber.toString();
+                            selectedNumber = selectedNumber.replace("-", "");
+                            editText.setText(selectedNumber);
+                        }
+
+                        if (phoneNumber.length() == 0) {
+                            //no numbers found actions
+                        }
+                    }
+                    break;
+            }
+        } else {
+            //not ok actions
+        }
     }
 
     public void send(View view) {
